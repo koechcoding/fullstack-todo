3760570d8624748f37c00ffd67a71779ec97fca2
Kelvin Koech <koechkelvin2001@gmail.com>
2023-06-17 22:51:40 +0300

    configuration

---
+++ backend/src/Config/index.ts
@@ -0,0 +1,28 @@
+import sql from 'mssql'
+import { log } from 'console'
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({path:path.resolve(__dirname, '../../.env')})
+export const sqlConfig = {
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PWD as string,
+  database: process.env.DB_NAME as string,
+  server: process.env.DB_SERVER as string,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: false, // for azure
+    trustServerCertificate: false // change to true for local dev / self-signed certs
+  }
+}
+
+//Test DB
+// sql.connect(sqlConfig).then(pool=>{
+//     if(pool.connected){
+//         console.log("I'm Connected  the database");
+        
+//     }
+// })
